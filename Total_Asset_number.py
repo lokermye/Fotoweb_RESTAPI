bcf34cc6aa2b9308203df14fd2c7b7b86a693e02
lokermye <39966285+lokermye@users.noreply.github.com>
2019-01-29 21:38:44 +0100

    Update Total_Asset_number.py

--- Total_Asset_number.py
+++ Total_Asset_number.py
@@ -1,6 +1,6 @@
 #print(json.dumps(data, indent=2))
-# importing the requests library 
-import requests 
+# importing the requests library
+import requests
 #import re for regex
 import re
 #importing the pymongo library
@@ -9,19 +9,6 @@ import re
 #import os
 #Accept: application/vnd.fotoware.assetlist+json
 import json
-#delete
-url = "https://kulturminnebilder.ra.no/fotoweb/archives/5012-Byer/"
-
-headers = {
-    'Accept': "application/vnd.fotoware.assetlist+json",
-    'cache-control': "no-cache",
-    'Postman-Token': "11cbd20d-efcf-4093-b61c-5320529df5a8"
-    }
-
-response = requests.request("GET", url, headers=headers)
-
-#print(response.text)
-#delete
 
 print('test of requests module')
 #Archive list#
@@ -29,24 +16,13 @@ url="https://kulturminnebilder.ra.no/fotoweb/archives/"
 headers = {
     'Accept': "application/vnd.fotoware.collectionlist+json",
     'cache-control': "no-cache",
-    'Postman-Token': "ca8bd137-997c-4994-ba95-de936b3ccc91"
+
     }
 r =  requests.request("GET", url, headers=headers)
-#Archive list#
-
-#archive
-
-'/fotoweb/archives/5000-Wirefeed/?q=Tower&101=London'
-
-
-
-
-
-
 #Load json#
 data = json.loads(r.content)
 
-#print some returned data 
+#print some returned data
 print('Avalable Archives:')
 print(len(data['data']))
 
@@ -54,11 +30,11 @@ print(len(data['data']))
 for archive in data['data']:
   #  name= archive['data']['name']
   print(archive['name'])
-  print(archive['href']) 
+  print(archive['href'])
 ######DO Some Shit WHit The Data####
 
   #api reqest archive list#
-  archive_url=('https://kulturminnebilder.ra.no'+(archive['href']))    
+  archive_url=('https://kulturminnebilder.ra.no'+(archive['href']))
   print(archive_url)
   #import requests
 
@@ -67,43 +43,50 @@ for archive in data['data']:
   headers_AL = {
     'Accept': "application/vnd.fotoware.assetlist+json",
    'cache-control': "no-cache",
-    'Postman-Token': "11cbd20d-efcf-4093-b61c-5320529df5a8"
+
    }
 
   AL_response = requests.request("GET", archive_url, headers=headers_AL)
   AL_Json=json.loads(AL_response.content)
   #print((AL_Json['paging']['next']))
   urlpage=('https://kulturminnebilder.ra.no'+((AL_Json['paging']['next'])))
+  lastpageurl=('https://kulturminnebilder.ra.no'+((AL_Json['paging']['last'])))
   #OWERWRTIE IN WHILE LOOP
   #print(urlpage)
-  
+
   print((AL_Json['paging']['last']))
   lastpage=(AL_Json['paging']['last'])
-  #go thorug all pages merge json to do a doc count :) 
-  doc_count=0
-  page=" "
+  page=' '
+  #use first page to
   while page  !=lastpage:
-   
+   print("--------------")
    next_al= requests.request("GET", urlpage, headers=headers_AL )
    next_aljson=json.loads(next_al.content)
-   #print(next_al.response)
-   page=(next_aljson['paging']['next'])
-   numberfromstring = ((re.findall('p=\d{1,3}',page))[0])
-   print( numberfromstring)
-   numberfromstring =int(numberfromstring.replace('p=',''))
+   page=(next_aljson['paging']['last'])
+   print urlpage
+   print lastpageurl
+   if urlpage != lastpageurl :
+       numberfromstring = ((re.findall('p=\d{1,3}',page))[0])
+       numberfromstring =int(numberfromstring.replace('p=',''))
+
+   #page=(next_aljson['paging']['last'])
+   urlpage=('https://kulturminnebilder.ra.no'+((next_aljson['paging']['last'])))
+
+
+   test=(next_aljson['data'][1]['filename'])
+   data=(next_aljson['data'])
+   print(test)
+   print(len(data) )
    print(numberfromstring)
-   
+
    print('number of pages:')
    print((numberfromstring+1))
    print('tot number of assets:')
-   #print((numberfromstring*25))
    total=(numberfromstring+1)*25
    print(total)
-   page=(next_aljson['paging']['last'])
-   urlpage=('https://kulturminnebilder.ra.no'+((next_aljson['paging']['next'])))
-   
-   
-   
+   #end
+
+
 
 
 
